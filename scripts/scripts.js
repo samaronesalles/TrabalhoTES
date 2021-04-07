d8b52e9ab9a9544aa7ecd321cd9700deb36f4b8b
Samarone Salles <samaronesalles@gmail.com>
2021-04-07 08:00:40 -0300

    commit inicial

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const link = document.querySelector('.linkCrieConta');
+
+link.addEventListener("click", function () {
+    modalOverlay.classList.add('active');
+    modalOverlay.querySelector("iframe").src = "novaConta.html";
+});
+
+document.querySelector('.close-modal').addEventListener("click", function () {
+    modalOverlay.classList.remove('active');
+    modalOverlay.querySelector("iframe").src = "";
+});
+
